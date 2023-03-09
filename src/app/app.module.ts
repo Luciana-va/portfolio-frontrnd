71f19803e562d9e971619986365943398b61f3c6
Luciana <lucianay.vallejos@gmail.com>
2023-03-09 18:21:56 -0300

    Se colocó una imagen de fondo y se inició el trabajo en el encabezado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabeceraComponent } from './componentes/cabecera/cabecera.component';
+import { PieComponent } from './componentes/pie/pie.component';
+import { PrincipalComponent } from './componentes/principal/principal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceraComponent,
+    PieComponent,
+    PrincipalComponent
   ],
   imports: [
     BrowserModule,
